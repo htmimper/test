e1f806d706555fdc7da536af03b8bfc8d316d3ca
htmimper <htmimper@gmail.com>
2025-03-19 21:28:29 +0900

    test version01

---
+++ java script practice/script.js
@@ -0,0 +1,61 @@
+　document.write ("Hello World "); 
+ document.write("あいうえお<br>");
+ document.write("かきくけこ<br>");
+ document.write("さしすせそ<br>");
+ document.write("なにぬねの<br>はひふへほ<br>まみむめも");
+ document.write("<h1>あいうえお</h1>");
+ document.write("かきくけこ<br>");
+ document.write("さしすせそ<br>");
+/*JavaScriptとJavaは全く違うプロ
+グラミング言語です。
+間違えやすいので気を付けてください。
+*/
+alert("このコンテンツはダウンロードに時間がかかる可能性があります。");
+confirm("次のページへ進みますか？");
+ document.write("12345<br>");
+/*数値を表示するときはダブルクオーテーションはつけません。
+改行の時や、数値を文字として表示する時はダブルクオーテーションをつけます。
+*/
+ document.write(3.14);
+/*整数だけでなく、少数点を表示することも出来る。
+この3.14に関しては、改行なしのためダブルクオーテーションをつけていません。
+*/
+document.write("<br>")
+    var tel= "090-1234-5678";
+document.write(tel  );
+document.write("<br>" );
+    var name = "¥’♡高橋♡¥’";
+document.write(name );
+document.write("<br>" );
+    var a =3+3;
+document.write(a);
+/*” ”で囲むと、数式ではなく、文字列として認識される*/
+
+document.write("<br>" );
+    var a=10;
+ document.write(a++); 
+ document.write( "<br>"); 
+ document.write(a++); 
+ document.write( "<br>");
+ document.write(a++);
+
+document.write("<br>" );
+
+document.write("<h1>難しい</h1>");
+    var a=10;
+document.write(a--); 
+document.write("<br>"); 
+ document.write(a--);
+document.write("<br>"); 
+ document.write(a--);
+
+document.write("<h1>IF</h1>");
+document.write("<br>" );
+var　a = 15;
+if( a>10){document.write( "A");}
+document.write("<br>" );
+var　a = 7;
+if( a<10){document.write( "B");}
+
+
+ 
